40bbeafd52fc8aebae60cbaf60908f715159f293
João Ferreira <yFerreiraJoao@gmail.com>
2024-08-29 14:13:40 -0300

    Update labirinto.py

--- labirinto.py
+++ labirinto.py
@@ -1,1 +1,13 @@
+# Dado um labirinto no estilo abaixo, começando da primeira linha, encontre a saída
+
+# [[0, 0, 1, 0, 0, 0],
+#  [0, 1, 1, 0, 1, 0],
+#  [0, 1, 0, 0, 1, 0],
+#  [0, 1, 1, 1, 1, 0],
+#  [0, 0, 0, 1, 0, 0],
+#  [0, 1, 1, 1, 0, 0],
+#  [0, 1, 0, 1, 1, 0],
+#  [0, 1, 0, 0, 0, 0]]
+
+
 print("Hello World")
